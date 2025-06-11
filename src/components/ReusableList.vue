f7f8b9d8061cfbbe555fcde551ac59e17efbef49
Guilherme Bloemer <guilhermbloem@gmail.com>
2025-06-11 20:04:48 -0300

    feature: delete startup

--- src/components/ReusableList.vue
+++ src/components/ReusableList.vue
@@ -8,13 +8,13 @@
                 :group="{ name: 'kanban', pull: true, put: true }" class="space-y-3" item-key="id" @end="onDragEnd">
                 <template #item="{ element: item }">
                     <div class="bg-white p-3 rounded-lg shadow-sm border border-gray-200">
-                        <div class="flex justify-between items-center">
-                            <div>
-                                <h3 class="font-semibold text-gray-800 text-sm">
-                                    {{ item.name }}
+                        <div class="flex justify-between items-center w-full">
+                            <div class="max-w-2/3">
+                                <h3 class="font-semibold text-gray-800 text-sm max-w-full ">
+                                    <p class="overflow-hidden text-ellipsis">{{ item.name }}</p>
                                 </h3>
                                 <p class="text-xs text-gray-500 mt-1">
-                                    {{ item.description }}
+                                    <p class="overflow-hidden text-ellipsis">{{ item.description }}</p>
                                 </p>
                             </div>
                             <div v-if="user.profile == 'admin'" class="flex flex-col gap-1">
@@ -22,7 +22,7 @@
                                     class="text-xs bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600">
                                     Editar
                                 </router-link>
-                                <button @click="() => props.onDelete(item)"
+                                <button @click="() => handleDelete(item.id)"
                                     class="text-xs bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600">
                                     Excluir
                                 </button>
@@ -40,6 +40,7 @@ import { defineProps } from 'vue';
 import draggable from 'vuedraggable';
 import { computed } from 'vue'
 import { useStore } from 'vuex'
+import { toast } from 'vue3-toastify';
 
 const props = defineProps({
     items: {
@@ -97,4 +98,35 @@ function onDragEnd(event) {
         });
 
 }
+
+const handleDelete = (id) => {
+    if (confirm('Tem certeza que deseja excluir este item?')) {
+        onDelete(id);
+    }
+};
+
+const onDelete = (id) => {
+    console.log(`Excluindo item com ID: ${id}`);
+    fetch(`http://127.0.1:8000/startups/${id}/`, {
+        method: 'DELETE',
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao excluir o item');
+            }
+            toast.success('Item excluído com sucesso!', {
+                hideProgressBar: false,
+                autoClose: 1000,
+                position: toast.POSITION.BOTTOM_RIGHT,
+                pauseOnHover: true,
+            });
+
+            setTimeout(() => {
+                window.location.reload();
+            }, 2000);
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
+};
 </script>
